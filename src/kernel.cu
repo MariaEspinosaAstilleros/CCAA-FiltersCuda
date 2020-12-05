c329ed0b6afdff0ce37e85babcccbe07af87b64d
MariaEspinosaAstilleros <maria.espinosa6@alu.uclm.es>
2020-12-05 13:05:21 +0100

    changes updated

--- src/kernel.cu
+++ src/kernel.cu
@@ -1,6 +1,12 @@
 #include <iostream>
 
+#include <opencv2/imgcodecs.hpp>
+
 //kernel sobel filter
-__global__ void sobel_filter(){
+__global__ void kernel_sobel(unsigned char* src_img, unsigned char* dst_img, unsigned int width, unsigned int heigth){
+
+    /*Gradients of the sobel filter*/
+    int gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}}; 
 
 }
--- src/sobel.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-//#include <opencv4/opencv2/imgcodecs.hpp>
-
-/*Gradients of the sobel filter*/
-int gx_g[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
-int gy_g[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}}; 
-
-
-
