08646993a42851dec06d320c53a9936384d726a9
María Espinosa Astilleros <maria.espinosa6@alu.uclm.es>
2020-12-23 20:22:52 +0100

    Update kernel_photo.cu

--- src/kernel_photo.cu
+++ src/kernel_photo.cu
@@ -13,10 +13,9 @@
 
 #define BLOCK_SIZE 32
 #define GRID_SIZE 128
-#define LIM 4
 
 /*Kernel*/
-__global__ void kernel_sobel_photo(char* src_img, char* dst_img, int width_img , int height_img){
+__global__ void kernel_convolution_sobel(char* src_img, char* dst_img, int width_img, int height_img){
 
     //Gradients of the sobel filter
     int gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
@@ -25,16 +24,18 @@ __global__ void kernel_sobel_photo(char* src_img, char* dst_img, int width_img ,
     int num_col = blockIdx.x * blockDim.x + threadIdx.x;
     int num_row = blockIdx.y * blockDim.y + threadIdx.y;
 
-    int index = num_row * width_img + num_col;
+    int index = (num_row * width_img) + num_col;
 
     if(num_col < (width_img - 1) && num_row < (height_img - 1)) {
-        char grad_x = ((src_img[index] * gx[0][2]) + (2 * (src_img[index] * gx[1][2])) + (src_img[index] * gx[2][2])) - 
-        ((src_img[index] * gx[0][0]) + (2 * (src_img[index] * gx[1][0])) + (src_img[index] * gx[2][0]));
+        int grad_x = (src_img[index] * gx[0][0]) + (src_img[index] * gx[0][1]) + (src_img[index] * gx[0][2]) +
+                     (src_img[index + 1] * gx[1][0]) + (src_img[index + 1] * gx[1][1]) + (src_img[index + 1] * gx[1][2]) +
+                     (src_img[index + 2] * gx[2][0]) + (src_img[index + 2] * gx[2][1]) + (src_img[index + 2] * gx[2][2]);
 
-        char grad_y = ((src_img[index] * gy[2][0]) + (2 * (src_img[index] * gy[2][1])) + (src_img[index] * gy[2][2])) - 
-        ((src_img[index] * gy[0][0]) + (2 * (src_img[index] * gy[0][1])) + (src_img[index] * gy[0][2]));
-        
-        dst_img[index] = sqrtf((grad_x * grad_x) + (grad_y * grad_y));
+        int grad_y = (src_img[index] * gy[0][0]) + (src_img[index] * gy[0][1]) + (src_img[index] * gy[0][2]) +
+                     (src_img[index + 1] * gy[1][0]) + (src_img[index + 1] * gy[1][1]) + (src_img[index + 1] * gy[1][2]) +
+                     (src_img[index + 2] * gy[2][0]) + (src_img[index + 2] * gy[2][1]) + (src_img[index + 2] * gy[2][2]);
+
+        dst_img[index] = (char) sqrtf(grad_x * grad_x + grad_y * grad_y);
     }
 }
 
@@ -62,10 +63,6 @@ cudaError_t testCuErr(cudaError_t dst_img){
     return dst_img;
 }
 
-int iDivUp(int a, int b){
-	return ((a % b) != 0) ? (a / b + 1) : (a / b);
-}
-
 void sobelFilterPhoto(cv::Mat *src_img){
 
     //check input image
@@ -80,7 +77,7 @@ void sobelFilterPhoto(cv::Mat *src_img){
     int img_size = src_img->rows * src_img->cols * sizeof(char);
 
     dim3 threadsPerBlock(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 numBlocks(iDivUp(src_img->rows, BLOCK_SIZE), iDivUp(src_img->cols, BLOCK_SIZE));
+    dim3 numBlocks(GRID_SIZE, GRID_SIZE);
 
     testCuErr(cudaMalloc((void**)&dev_src, img_size));
     testCuErr(cudaMalloc((void**)&dev_sobel, img_size));
@@ -92,7 +89,7 @@ void sobelFilterPhoto(cv::Mat *src_img){
     auto start = std::chrono::high_resolution_clock::now();
 
     //call kernel
-    kernel_sobel_photo<<<numBlocks,threadsPerBlock>>>(dev_src, dev_sobel, src_img->step1(), src_img->rows);
+    kernel_convolution_sobel<<<numBlocks,threadsPerBlock>>>(dev_src, dev_sobel, src_img->step1(), src_img->rows);
     testCuErr(cudaGetLastError()); testCuErr(cudaDeviceSynchronize());
 
     //end time 
